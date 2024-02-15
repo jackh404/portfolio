a3254135afeeee5f6a8348d55dac26a4db9dcfb9
Jack Henza <jackhenza@gmail.com>
2024-02-15 15:20:37 -0500

    add portfolio itself to projects page

--- app/projects/ProjectCard.tsx
+++ app/projects/ProjectCard.tsx
@@ -3,7 +3,7 @@ import { FaGithub, FaYoutube } from "react-icons/fa";
 
 const ProjectCard = (project: Project) => {
   return (
-    <div className="card w-96 bg-neutral shadow-xl text-neutral-content">
+    <div className="card max-w-md bg-neutral shadow-md text-neutral-content shadow-neutral-700">
       <figure>
         <a href={project.link} target="_blank">
           <img
@@ -18,15 +18,19 @@ const ProjectCard = (project: Project) => {
           <h2 className="card-title hover:underline">{project.title}</h2>
         </a>
         <p>{project.description}</p>
-        <div className="card-actions justify-end mt-2">
+        <div className="card-actions mt-2">
           {project.skills.map(skill => (
             <div className="badge badge-outline" key={skill}>
               {skill}
             </div>
           ))}
-          <a href={project.demo} target="_blank">
-            <FaYoutube className="w-8 h-8 mt-2 mr-2" />
-          </a>
+        </div>
+        <div className="flex justify-end gap-4">
+          {project.demo ? (
+            <a href={project.demo} target="_blank">
+              <FaYoutube className="w-8 h-8 mt-2 mr-2" />
+            </a>
+          ) : null}
           <a href={project.github} target="_blank">
             <FaGithub className="w-8 h-8 mt-2 mr-2" />
           </a>
--- app/projects/page.tsx
+++ app/projects/page.tsx
@@ -16,14 +16,23 @@ export default function Projects() {
       image: "/ecoinsight.png",
       link: "https://ecoinsight.vercel.app",
       github: "https://github.com/jackh404/ecoinsight",
-      skills: ["React", "Flask SQLAlchemy", "TypeScript"],
+      skills: ["React.JS", "Flask SQLAlchemy", "TypeScript"],
       demo: "https://youtu.be/2Qq7R_hVwkk",
+    },
+    {
+      title: "Portfolio",
+      description:
+        "This portfolio website is a simple Next.JS app aimed at aquanting visitors with my personal story and my work in software development. It is coded in TypeScript with Tailwind CSS and DaisyUI for styling.",
+      image: "/portfolio.png",
+      link: "https://jackhenza.com",
+      github: "https://github.com/jackh404/portfolio",
+      skills: ["Next.JS", "TypeScript", "Tailwind CSS"],
     },
   ];
   return (
     <div>
       <h1 className="text-3xl p-6">My Projects</h1>
-      <div className="grid grid-cols-1 sm:grid-cols-2 md:grid-cols-3">
+      <div className="grid grid-cols-1 md:grid-cols-2 xl:grid-cols-3 gap-4 justify-items-center">
         {projects.map(project => (
           <ProjectCard key={project.title} {...project} />
         ))}
